e1a7b10d178ba48f870d8c47bf4e9931e9af77c0
Dan Mircea Pinte <dan.pinte@siemens.com>
2022-08-01 09:39:44 +0300

    Add project files.

---
+++ ReactMvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactMvc5.Startup))]
+namespace ReactMvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
